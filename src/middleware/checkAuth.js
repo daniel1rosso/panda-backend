8135cbca832f6ee1bc745312835f323318b48190
DESKTOP-N84JKO5\damian <dpramparo@startuniversity.net>
2021-06-13 17:41:42 -0300

    Update checkAuth.js

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/UsuarioModels.js')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     try {
     	 
         const token = req.headers.authorization.split(" ")[1];
