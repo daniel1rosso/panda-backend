7d3a5a8b209b42987923712d441a7e718db66363
DESKTOP-N84JKO5\damian <dpramparo@startuniversity.net>
2021-06-13 17:48:41 -0300

    Update checkAuth.js

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -5,7 +5,8 @@ module.exports = async (req, res, next) => {
     try {
     	 
         const token = req.headers.authorization.split(" ")[1];
-        const user = await User.findOne({ _id: token._id, 'tokens.token': token })
+        const data = await User.findOne({ _id: token._id, 'tokens.token': token })
+        console.log(data);
         const decoded = (token == user.token) ? true : false
         res.userData = decoded;
         (decoded) ? next() : res.status(401).json({ message: "Auth failed",  success: 0 })
@@ -13,3 +14,22 @@ module.exports = async (req, res, next) => {
         res.status(401).json({ message: "Auth failed",  success: 0 })
     }
 }
+
+
+const auth = async(req, res, next) => {
+   const token = req.header('Authorization').replace('Bearer ', '')
+   const data = jwt.verify(token, process.env.JWT_KEY)
+   try {
+      const user = await User.findOne({ _id: data._id, 'tokens.token': token })
+      if (!user) {
+         throw new Error()
+      }
+      req.user = user
+      req.token = token
+      next()
+   } catch (error) {
+      res.status(401).send({ error: 'Not authorized to access this resource' })
+   }
+
+}
+module.exports = auth
