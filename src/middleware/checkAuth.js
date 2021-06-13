ae3ce4eb14e2e1876923849d0352a58ca2548e37
DESKTOP-N84JKO5\damian <dpramparo@startuniversity.net>
2021-06-13 18:21:01 -0300

    Update checkAuth.js

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,35 +1,17 @@
 const jwt = require('jsonwebtoken')
-const User = require('../models/UsuarioModel')
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
+    //try {
     try {
-    	 
         const token = req.headers.authorization.split(" ")[1];
-        const data = await User.findOne({ _id: token._id, 'tokens.token': token })
-        console.log(data);
-        const decoded = (token == user.token) ? true : false
+        //const decoded = jwt.verify(token, process.env.token)
+        const decoded = (token == process.env.token) ? true : false
         res.userData = decoded;
         (decoded) ? next() : res.status(401).json({ message: "Auth failed",  success: 0 })
+    //} catch (error) {
     } catch (error) {
-        res.status(401).json({ message: "Auth failed",  success: 0 })
+        //res.status(401).json({ message: "Auth failed",  success: 0 })
+    //}
     }
 }
 
-
-const auth = async(req, res, next) => {
-   const token = req.header('Authorization').replace('Bearer ', '')
-   const data = jwt.verify(token, process.env.JWT_KEY)
-   try {
-      const user = await User.findOne({ _id: data._id, 'tokens.token': token })
-      if (!user) {
-         throw new Error()
-      }
-      req.user = user
-      req.token = token
-      next()
-   } catch (error) {
-      res.status(401).send({ error: 'Not authorized to access this resource' })
-   }
-
-}
-module.exports = auth
