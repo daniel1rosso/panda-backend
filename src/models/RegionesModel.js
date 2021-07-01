ffdadaabb42dfbf9101b22607a5d730ecc7de799
Daniel Rosso <38544140+daniel1rosso@users.noreply.github.com>
2021-06-30 23:34:04 -0300

    changes

--- src/models/RegionesModel.js
+++ src/models/RegionesModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const RolSchema = mongoose.Schema({
     name: { type: String, required: true },
-    regions: { type: Array, required: true }
+    regions: { type: Array, required: true },
+    number: { type: Number, required: true },
 })
 
 module.exports = mongoose.model('regiones_comunas', RolSchema);
